197c84a94ca0c33f4b5a4076a57df4bf77db270c
Juan Araque <juan.araque@correo.unimet.edu.ve>
2023-02-05 14:35:43 -0400

    hero terminado

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+//Evento click en el menu
+
 const hamburger = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-menu')
+const mobile_menu = document.querySelector('.mobile-menu') 
 
 hamburger.addEventListener('click', () => {
     mobile_menu.classList.toggle('is-open');
 });
+
+// Carrusel de fotos
+
+let counter = 1;
+setInterval(() => {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 5000);
